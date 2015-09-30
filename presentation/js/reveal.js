f9607622243c18ef453628594c4b5d9d3282ad38
Lauren Ancona <lauren.ancona@phila.gov>
2015-09-30 13:22:36 -0700

    .

--- presentation/js/reveal.js
+++ presentation/js/reveal.js
@@ -35,8 +35,8 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 960,
-			height: 700,
+			width: 1280,
+			height: 720,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.1,
